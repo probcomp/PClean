1d9109e307b8ad0b65a259602f7a0a4a6ce219b2
alex-lew <alexl.mail@gmail.com>
2021-03-06 00:21:01 -0500

    Fix load_data for rents

--- experiments/rents/load_data.jl
+++ experiments/rents/load_data.jl
@@ -2,16 +2,18 @@ using CSV
 using DataFrames: DataFrame
 
 # Load data
-table = CSV.File("datasets/dirty_rents.csv") |> DataFrame
-table[!, :CountyKey] = map(x -> "$(x[1])$(split(x)[1][end])", table[!, :County])
+dataset = "rents"
+dirty_table = CSV.File("datasets/$(dataset)_dirty.csv") |> DataFrame
+clean_table = CSV.File("datasets/$(dataset)_clean.csv") |> DataFrame
 
-possibilities = Dict(c => Set() for c in unique(table.CountyKey))
-for r in eachrow(table)
+dirty_table[!, :CountyKey] = map(x -> "$(x[1])$(split(x)[1][end])", dirty_table[!, :County])
+
+possibilities = Dict(c => Set() for c in unique(dirty_table.CountyKey))
+for r in eachrow(dirty_table)
   push!(possibilities[r[:CountyKey]], r[:County])
 end
 possibilities = Dict(c => [possibilities[c]...] for c in keys(possibilities))
 
-const states = unique(filter(x -> !ismissing(x), table.State))
+const states = unique(filter(x -> !ismissing(x), dirty_table.State))
 const room_types = ["studio", "1br", "2br", "3br", "4br"]
 
-clean_table = CSV.File("datasets/clean_rents.csv") |> DataFrame

6739ecfdb66c0dda63e6c87251bbc8c74d4468db
Ian Limarta <ian.limarta@gmail.com>
2024-07-22 17:57:48 -0500

    Basic extraction

--- experiments/doctor/experimental.jl
+++ experiments/doctor/experimental.jl
@@ -11,6 +11,7 @@ const CREDENTIALS = possibilities["Credential"]
 const SCHOOLS = possibilities["Medical school name"]
 const BUSINESSES = possibilities["Organization legal name"]
 const LASTNAMES = possibilities["Last Name"]
+const ADDRS = possibilities["Line 1 Street Address"]
 
 PClean.@model PhysicianModel begin
   @class School begin
@@ -41,7 +42,10 @@ PClean.@model PhysicianModel begin
   end
 
   @class BusinessAddr begin
-    addr ~ Unmodeled(); #@guaranteed addr
+    @learned addr_proportions::ProportionsParameter{3.0}
+    addr ~ ChooseProportionally(ADDRS, addr_proportions)
+    # addr ~ Unmodeled(); #@guaranteed addr
+
     addr2 ~ Unmodeled(); #@guaranteed addr2
     zip ~ StringPrior(3, 10, String[]); #@guaranteed zip
 
@@ -96,6 +100,10 @@ trace = PClean.PCleanTrace(PhysicianModel, table);
 
 PClean.save_results("results", "physician", trace, observations)
 
+existing_physicians = keys(trace.tables[:Physician].rows)
+existing_businesses = keys(trace.tables[:BusinessAddr].rows) 
+existing_observations = Set([(row[PClean.resolve_dot_expression(trace.model,:Obs, :p)], row[PClean.resolve_dot_expression(trace.model, :Obs, :a)]) for (id, row) in trace.tables[:Obs].rows])
+
 # gilman = filter(pair-> (row = last(pair); row[5] == String31("STEVEN") && row[6] == String31("GILMAN")), trace.tables[:Physician].rows)
 function find_person(trace; firstname=nothing, lastname=nothing)
   firstname === nothing && lastname === nothing && error("Specify at least first or last")
@@ -132,10 +140,10 @@ row_id = rand(10000:20000)
 
 row_trace = Dict{PClean.VertexID, Any}()
 row_trace[PClean.resolve_dot_expression(trace.model, :Obs, :(p.first))] = "STEVEN"
-row_trace[PClean.resolve_dot_expression(trace.model, :Obs, :(p.last))] = "GILMAN"
+# row_trace[PClean.resolve_dot_expression(trace.model, :Obs, :(p.last))] = "GILMAN"
 # row_trace[PClean.resolve_dot_expression(trace.model, :Obs, :(p.school.name))] = "ALBANY MEDICAL COLLEGE OF UNION UNIVERSITY"
 row_trace[PClean.resolve_dot_expression(trace.model, :Obs, :(a.city.c2z3))] = "CA-170"
-row_trace[PClean.resolve_dot_expression(trace.model, :Obs, :(a.addr))] = "429 N 21ST ST"
+# row_trace[PClean.resolve_dot_expression(trace.model, :Obs, :(a.addr))] = "429 N 21ST ST"
 row_trace[PClean.resolve_dot_expression(trace.model, :Obs, :(a.addr2))] = ""
 row_trace[PClean.resolve_dot_expression(trace.model, :Obs, :(a.legal_name))] = "SPIRIT PHYSICIAN SERVICES INC"
 row_trace[PClean.resolve_dot_expression(trace.model, :Obs, :(a.zip))] = String15("170112202")
@@ -146,16 +154,18 @@ row_trace[PClean.resolve_dot_expression(trace.model, :Obs, :(p.specialty))] = "D
 obs = trace.tables[:Obs].observations
 obs[row_id] = row_trace
 
-specialty_samples = String[]
-last_name_samples = String[]
-physician_ids = Symbol[]
+samples = Dict{Int64,Any}[]
 for _ in 1:10
-  PClean.run_smc!(trace, :Obs, row_id, PClean.InferenceConfig(30,5))
+  PClean.run_smc!(trace, :Obs, row_id, PClean.InferenceConfig(40,5))
+  push!(samples, copy(trace.tables[:Obs].rows[row_id]))
   # temp = find_spirit_service(trace)
   # display(temp)
   # business_addr_ids_different = symdiff(keys(spirit_service_instances), keys(temp))
   # println(business_addr_ids_different)
   # println(trace.tables[:Obs].rows[row_id][PClean.resolve_dot_expression(trace.model, :Obs, :a)])
+  println(trace.tables[:Obs].rows[row_id][PClean.resolve_dot_expression(trace.model, :Obs, :a)])
+  println(trace.tables[:Obs].rows[row_id][PClean.resolve_dot_expression(trace.model, :Obs, :(a.legal_name))])
+  println(trace.tables[:Obs].rows[row_id][PClean.resolve_dot_expression(trace.model, :Obs, :(a.addr))])
   println(trace.tables[:Obs].rows[row_id][PClean.resolve_dot_expression(trace.model, :Obs, :(a.city.name))])
   println(trace.tables[:Obs].rows[row_id][PClean.resolve_dot_expression(trace.model, :Obs, :p)])
   println(trace.tables[:Obs].rows[row_id][PClean.resolve_dot_expression(trace.model, :Obs, :(p.first))])
@@ -169,16 +179,52 @@ PClean.save_results("results", "physician", trace, observations)
 
 [row[PClean.resolve_dot_expression(trace.model, :Physician, :last)] for (id, row) in find_person(trace,firstname="STEVEN")]
 
-p_freq = countmap(physician_ids)
-gilman in keys(p_freq)
+function attribute_extractors(model::PClean.PCleanModel)
+    physician_attributes = Dict(
+      "npi"=>PClean.resolve_dot_expression(model, :Obs, :(p.npi)),
+      "first"=>PClean.resolve_dot_expression(model, :Obs, :(p.first)),
+      "last"=>PClean.resolve_dot_expression(model, :Obs, :(p.last)),
+      "degree"=>PClean.resolve_dot_expression(model, :Obs, :(p.degree)),
+      "speciality"=>PClean.resolve_dot_expression(model, :Obs, :(p.specialty)),
+      "school" => PClean.resolve_dot_expression(model, :Obs, :(p.school.name))
+    )
 
-specialty_freq = countmap(specialty_samples);
-l  = collect(specialty_freq);
-l[partialsortperm(l, 1:3, by=last, rev=true)]
+    business_attributes = Dict(
+      "legal_name" => PClean.resolve_dot_expression(model, :Obs, :(a.legal_name)),
+      "addr" => PClean.resolve_dot_expression(model, :Obs, :(a.addr)),
+      "addr2" => PClean.resolve_dot_expression(model, :Obs, :(a.addr2)),
+      "zip" => PClean.resolve_dot_expression(model, :Obs, :(a.zip)),
+      "city" => PClean.resolve_dot_expression(model, :Obs, :(a.city.name)),
+    )
 
-last_freq = countmap(last_name_samples);
-l  = collect(last_freq);
-l[partialsortperm(l, 1:3, by=last, rev=true)]
+    function attributes(row)
+      physician_attr = Dict(attribute=>row[id] for (attribute, id) in physician_attributes)
+      business_attr = Dict(attribute=>row[id] for (attribute, id) in business_attributes)
+      physician_id = row[PClean.resolve_dot_expression(model, :Obs, :p)]
+      business_id = row[PClean.resolve_dot_expression(model, :Obs, :a)]
+      return (physician_id, business_id), physician_attr, business_attr
+    end
+
+    return attributes
+end
+
+extractor = attribute_extractors(PhysicianModel)
+# sum(extractor(s)[1] in existing_physicians for s in samples)
+extractor(samples[5])
+ob = extractor(samples[5])[1]
+ob in existing_observations
+# extractor(samples[1])[1] in existing_physicians
+
+# p_freq = countmap(physician_ids)
+# gilman in keys(p_freq)
+
+# specialty_freq = countmap(specialty_samples);
+# l  = collect(specialty_freq);
+# l[partialsortperm(l, 1:3, by=last, rev=true)]
+
+# last_freq = countmap(last_name_samples);
+# l  = collect(last_freq);
+# l[partialsortperm(l, 1:3, by=last, rev=true)]
 
 # println(trace.tables[:Obs].rows[row_id])
 
